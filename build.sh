0ba67031d7d9e0f12f1ee3806e971a8127e1e290
Daniel Quanz <daniel.quanz@ecolytiq.com>
2022-01-28 10:21:31 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
- redoc-cli bundle ecolytiq-ecolytiq-api-1.1.0-swagger.yaml --options=theme.json
- mv redoc-static.html index.html
+redoc-cli bundle ecolytiq-ecolytiq-api-1.1.0-clean-swagger.yaml --options=theme.json
+mv redoc-static.html index.html
+
+redoc-cli bundle ecolytiq-ecolytiq-api-3.0.0-draft-swagger.yaml --options=theme.json
+mv redoc-static.html draft/index.html
  
