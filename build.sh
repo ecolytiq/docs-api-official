208f8bf6a42c9380588b9da3abc0b92e58f1b81f
Daniel Quanz <daniel.quanz@ecolytiq.com>
2022-06-19 17:55:19 +0200

    bump to 3.2.0. Add preview 3.3.0

--- build.sh
+++ build.sh
@@ -3,10 +3,14 @@
 redoc-cli bundle 2.4.0/openapi-2.4.0.yaml --options=theme.json
 cp redoc-static.html 2.4.0/index.html
 
-redoc-cli bundle 3.2.0-preview/openapi-3.2.0-preview.yaml --options=theme.json
-cp redoc-static.html 3.2.0-preview/index.html
-
 redoc-cli bundle 3.0.0/openapi-3.0.0.yaml --options=theme.json
 cp redoc-static.html 3.0.0/index.html
+
+redoc-cli bundle 3.3.0-preview/openapi-3.3.0.yaml --options=theme.json
+cp redoc-static.html 3.3.0-preview/index.html
+
+redoc-cli bundle 3.2.0/openapi-3.2.0.yaml --options=theme.json
+cp redoc-static.html 3.2.0/index.html
 mv redoc-static.html index.html
+
  
