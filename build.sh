41e361627e307063c21c8ae6a040cbbc1e225d71
Daniel Quanz <daniel.quanz@ecolytiq.com>
2022-09-15 05:05:29 +0200

    update api docs. Release new version

--- build.sh
+++ build.sh
@@ -6,11 +6,11 @@ cp redoc-static.html 2.4.0/index.html
 redoc-cli bundle 3.0.0/openapi-3.0.0.yaml --options=theme.json
 cp redoc-static.html 3.0.0/index.html
 
-redoc-cli bundle 3.3.0-preview/openapi-3.3.0.yaml --options=theme.json
-cp redoc-static.html 3.3.0-preview/index.html
-
 redoc-cli bundle 3.2.0/openapi-3.2.0.yaml --options=theme.json
 cp redoc-static.html 3.2.0/index.html
+
+redoc-cli bundle v3-2022-09-01/openapi.yaml --options=theme.json
+cp redoc-static.html v3-2022-09-01/index.html
 mv redoc-static.html index.html
 
  
