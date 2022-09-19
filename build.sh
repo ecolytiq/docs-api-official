c2a7485e6884863864fad7f9d9484a32071433f7
Daniel Quanz <daniel.quanz@ecolytiq.com>
2022-09-19 09:08:04 +0200

    add offset preview

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ cp redoc-static.html 3.0.0/index.html
 redoc-cli bundle 3.2.0/openapi-3.2.0.yaml --options=theme.json
 cp redoc-static.html 3.2.0/index.html
 
+redoc-cli bundle preview/openapi.yaml --options=theme.json
+cp redoc-static.html preview/index.html
+
 redoc-cli bundle v3-2022-09-01/openapi.yaml --options=theme.json
 cp redoc-static.html v3-2022-09-01/index.html
 mv redoc-static.html index.html
