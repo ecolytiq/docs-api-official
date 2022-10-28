4321d8020436fcf1fdc4a0ba09be7f4be3c7f4df
Carsten Schulze <carsten.schulze@ecolytiq.com>
2022-10-28 13:48:14 +0200

    QA-31 Disabled Google fonts and added ecolytiq fonts stylesheet to HTML output

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-redoc-cli bundle 2.4.0/openapi-2.4.0.yaml --options=theme.json
+redoc-cli build 2.4.0/openapi-2.4.0.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs
 cp redoc-static.html 2.4.0/index.html
 
-redoc-cli bundle 3.0.0/openapi-3.0.0.yaml --options=theme.json
+redoc-cli build 3.0.0/openapi-3.0.0.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs 
 cp redoc-static.html 3.0.0/index.html
 
-redoc-cli bundle 3.2.0/openapi-3.2.0.yaml --options=theme.json
+redoc-cli build 3.2.0/openapi-3.2.0.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs
 cp redoc-static.html 3.2.0/index.html
 
-redoc-cli bundle preview/openapi.yaml --options=theme.json
+redoc-cli build preview/openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs
 cp redoc-static.html preview/index.html
 
-redoc-cli bundle v3-2022-10-04/openapi.yaml --options=theme.json
+redoc-cli build v3-2022-10-04/openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs
 cp redoc-static.html v3-2022-10-04/index.html
 mv redoc-static.html index.html
 
