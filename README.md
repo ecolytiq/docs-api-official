2bd6a5e04b60c3141fbf66fcda8e76e09bd16451
Carsten Schulze <carsten.schulze@ecolytiq.com>
2022-12-20 18:02:11 +0100

    QA-44 Updated and hopefully fixed the postman collection to an easier working state

--- README.md
+++ README.md
@@ -2,8 +2,36 @@
 
 ## Requirements
 1. redoc-cli (`npm i -g redoc-cli`)
+2. openapi-to-postmanv2 (`npm i -g openapi-to-postmanv2`)
+   * must provide the `openapi2postmanv2` command.
+   * this can be achieved by creating an alias, e.g., 
+      ```console
+      alias openapi2postmanv2="/opt/homebrew/lib/node_modules/openapi-to-postmanv2/bin/openapi2postmanv2.js"
+      ```
+3. jq (`brew install jq`)
 
 ## Build Docs
 ```
 ./build.sh
 ```
+
+## Build Postman Collection from OpenAPI
+
+Requirements 2 and 3 must be met for this!
+
+Use the `buildPostman.sh` script in this repo:
+
+```console
+$ ./buildPostman.sh <INPUT> <OUTPUT>
+````
+
+This script does the following steps: 
+
+1. Parses the OpenAPI spec file and includes the `{{bearerToken}}` variable to all request headers
+2. Parse the OpenAPI spec file and set the proper values for the basic authentication for "Request Access Token"
+3. Parse the resulting JSON file and
+   1. Include a test that is executed when the "Request Access Token" endpoint is called. This enables Postman to store the access token in the `{{bearerToken}}` variable
+   2. update the request urls for the "Request Access Token" endpoint to `{{tokenUrl}}`
+   3. Set the correct values for `grant_type` `scope` 
+   4. Delete the "Postman" part where it says how to get and import the postman collection. I don't think that is useful information at this point anymore.
+
---
+++ buildPostman.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+# Use this script to convert an OpenAPI json file to a postman collection
+
+print_usage() {
+    echo "Usage: `basename $0` INPUT OUTPUT"
+}
+
+E_BADARGS=85
+
+if [ ! -n "$1" ] && [ ! -n "$2" ]
+then
+    print_usage
+    exit $E_BADARGS
+fi
+
+E_FILENOTFOUND=2
+
+if [ ! -f $1 ]
+then 
+    echo "Input file $1 not found"
+    print_usage 
+    exit $E_FILENOTFOUND
+fi
+
+INPUT=$1
+OUTPUT=$2
+
+TEMP1=$(mktemp)
+
+# TEMP2=$(mktemp)
+
+# TEMP3=$(mktemp)
+
+openapi2postmanv2 -s $INPUT -o $TEMP1 -p -O folderStrategy=Tags,optimizeConversion=false,stackLimit=20
+
+sed -i "" -e "s/<Bearer Token>/{{bearerToken}}/g"  \
+    -e "s/<Basic Auth Username>/{{clientId}}/g" \
+    -e "s/<Basic Auth Password>/{{clientSecret}}/g" $TEMP1
+
+jq '(.item[] 
+        | select(.name=="Authentication").item[] 
+        | select(.name="Request Access Token")).event=
+            [ 
+                { 
+                    "listen": "test", 
+                    "script": {
+                        "exec": [ 
+                            "pm.collectionVariables.set(\"bearerToken\", pm.response.json().access_token);" 
+                        ], 
+                        "type": "text/javascript" 
+                    } 
+                } 
+            ]
+    |(.item[] 
+        | select(.name=="Authentication").item[] 
+        | select(.name="Request Access Token")).url=
+            {
+                "raw": "{{tokenUrl}}", 
+                "host": ["{{tokenUrl}}"]
+            }
+    |(.item[]
+        | select(.name=="Authentication").item[]
+        | select(.name=="Request Access Token").request.body.urlencoded[] 
+        | select(.key=="grant_type")).value="client_credentials"
+    |(.item[]
+        | select(.name=="Authentication").item[]
+        | select(.name=="Request Access Token").request.body.urlencoded[] 
+        | select(.key=="grant_type")).type="text" 
+    |(.item[]
+        | select(.name=="Authentication").item[]
+        | select(.name=="Request Access Token").request.body.urlencoded[] 
+        | select(.key=="scope")).value="all"
+    |(.item[]
+        | select(.name=="Authentication").item[]
+        | select(.name=="Request Access Token").request.body.urlencoded[] 
+        | select(.key=="scope")).type="text"
+    |del(.item[] | select(.name == "Postman"))' $TEMP1 > $OUTPUT
+rm $TEMP1
+
+# jq '(.item[] | select(.name=="Authentication").item[] | select(.name="Request Access Token")).url={"raw": "{{tokenUrl}}", "host": ["{{tokenUrl}}"]}' $TEMP2 > $TEMP3
+# rm $TEMP2
+
+# jq 'del(.item[] | select(.name == "Postman"))' $TEMP3 > $OUTPUT
+# rm $TEMP3
