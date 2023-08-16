bc82c5b3393505495e87c2e701048f06f73a43eb
AkbarKazimov8 <akbar.kazimov@ecolytiq.com>
2023-08-16 22:03:58 +0200

    Fixes based on comments and user feedback

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-version=v3-2023-08-16
+version=v3-2023-08-17
 
-npx redoc-cli@0.13.21 build openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs --output index.html
+npx @redoc-cli@0.13.21 build openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs --output index.html
 bash ./buildPostman.sh openapi.yaml ecolytiq_Sandbox.postman_collection.json
 
 mkdir -p "${version}"/postman
