a5de1c699b69c809e64b02bbae1e4d43efedde62
DanielGronau <daniel.gronau@gmx.de>
2023-10-05 13:44:43 +0200

    v3-2023-10-04 initial commit

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=v3-2023-09-27
+version=v3-2023-10-04
 
 npx redoc-cli@0.13.21 build openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs --output index.html
 bash ./buildPostman.sh openapi.yaml ecolytiq_Sandbox.postman_collection.json
