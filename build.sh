dfad932cb9f3d2d0f70ed7878a9ec5f1e0091b47
Nico Duldhardt <nduldhardt@gmail.com>
2024-02-13 08:12:46 +0100

    update build.sh and push docs vor v3-2024-02-12

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=v3-2023-12-07
+version=$(awk '/version:/{print $2; exit}' openapi.yaml)
 
 npx redoc-cli@0.13.21 build openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs --output index.html
 bash ./buildPostman.sh openapi.yaml ecolytiq_Sandbox.postman_collection.json
