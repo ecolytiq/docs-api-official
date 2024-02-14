d824de7107394baf113f7aeea2c6ff658c4cdb91
Nico Duldhardt <nduldhardt@gmail.com>
2024-02-14 04:45:43 +0100

    modify build.sh to stop historization

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
 
-version=v3-2023-12-07
-
 npx redoc-cli@0.13.21 build openapi.yaml --options=theme.json --disableGoogleFont -t ecolytiq.hbs --output index.html
 bash ./buildPostman.sh openapi.yaml ecolytiq_Sandbox.postman_collection.json
-
-mkdir -p "${version}"/postman
-cp openapi.yaml "${version}"/openapi.yaml
-cp index.html "${version}"/index.html
-cp ecolytiq_Sandbox.postman_collection.json "${version}"/postman/ecolytiq_Sandbox.postman_collection.json
