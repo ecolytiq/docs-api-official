1e875e3a2ded8ce84a026823a129d1f3bfb7c1bc
Nico Duldhardt <nduldhardt@gmail.com>
2024-04-25 17:48:02 +0200

    build api docs by pulling openapi.yaml from gateway and action service

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-npx @redocly/cli@1.11.0 build-docs openapi.yaml --disableGoogleFont --template ecolytiq.hbs --output index.html --config redocly.yaml
+npx -y @redocly/cli@1.12.0 join openapi-gateway.yaml openapi-action.yaml --without-x-tag-groups -o openapi.yaml
+npx -y @redocly/cli@1.12.0 build-docs openapi.yaml  --disableGoogleFont --template ./ecolytiq.hbs --output=index.html --config=./redocly.yaml
 bash ./buildPostman.sh openapi.yaml ecolytiq_Sandbox.postman_collection.json
